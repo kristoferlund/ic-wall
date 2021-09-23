1f3f27bc9e75c7677daf7f6477936502acd3251a
awakexzxy <awakexzxy@163.com>
2021-09-23 10:48:08 +0800

    feat: Add ENS reverse resolution.

--- src/components/wall/UserInfo.tsx
+++ src/components/wall/UserInfo.tsx
@@ -3,6 +3,8 @@ import { ReactComponent as UserIcon } from "@/svg/user-solid.svg";
 import { Principal } from "@dfinity/principal";
 import React from "react";
 import { useRecoilValue } from "recoil";
+import useENSName from "@/hooks/useENSName";
+import { shortenAddress } from "@/utils/index";
 
 interface Props {
   principal: Principal;
@@ -10,12 +12,14 @@ interface Props {
 
 const UserInfoInner = ({ principal }: Props) => {
   const profile = useRecoilValue(ProfileByPrincipal({ principal }));
+  const { ENSName } = useENSName(profile?.address ?? undefined);
+
   if (profile?.name)
     return (
       <div className="pb-10 text-green-500">
         <UserIcon className="inline-block h-20 pb-3 mr-2" />
         <div className="mb-2 text-xl text-bold">{profile.name}</div>
-        <div className="text-sm">ETH: {profile.address}</div>
+        <div className="text-sm">ETH: {ENSName || shortenAddress(profile.address)}</div>
         <div className="text-sm">IC: {principal.toString()}</div>
       </div>
     );
